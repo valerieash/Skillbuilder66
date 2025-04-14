70206ac25ad7839f56d0449f5391c366bf98380e
valerieash <valerieash4@gmail.com>
2025-04-13 23:52:05 -0400

    complete

--- src/main/java/SkillBuilder6.java
+++ src/main/java/SkillBuilder6.java
@@ -5,65 +5,106 @@
  * @author (you)
  * @version (a version number or a date)
  */
-public class SkillBuilder6
-{
+public class SkillBuilder6 {
     /**
      * Calculates the prefix average of array data and returns the prefex average
      * in a new array.  The parameter data is never touched and left intact.
      *
      * @param data array of double values on which to calculate the prefix average.
      * @return returns an array containing the prefix average values calculated
-     *         from data.
+     * from data.
      */
-    public static double[] prefixAverage(double[] data)
-    {
-        //TODO: replace this line with your code.
+    public static double[] prefixAverage(double[] data) {
+        double[] prefixAverageArray = new double[data.length];
+        double cumulativeSum = 0;
+
+        for (int currentIndex = 0; currentIndex < data.length; currentIndex++) {
+            cumulativeSum += data[currentIndex];
+            prefixAverageArray[currentIndex] = cumulativeSum / (currentIndex + 1);
+        }
+        return prefixAverageArray;
     }
 
     /**
      * Finds the location in array anArray where value is located.  If anArray
      * does not contain an element equal to value a -1 is returned; otherwise a
      * positive or zero index value is returned.
+     *
      * @param searchValue value to look for in the array
-     * @param anArray array in which to look for a value
+     * @param anArray     array in which to look for a value
      * @returns index of the value in the array; -1 otherwise.
      */
-    public static int  indexOf(int searchValue, int[] anArray)
-    {
-        //TODO: replace this line with your code.
+    public static int indexOf(int searchValue, int[] anArray) {
+        for (int currentIndex = 0; currentIndex < integerArray.length; currentIndex++) {
+            if (integerArray[currentIndex] == searchValue) {
+                return currentIndex;
+            }
+        }
+        return -1;
     }
 
     /**
      * Finds the location in array anArray where string s is located.  If anArray
      * does not contain an element equal to s a -1 is returned; otherwise a
      * positive or zero index value is returned.
-     * @param s a string to look for in the array
+     *
+     * @param s       a string to look for in the array
      * @param anArray array in which to look for a value
      * @returns index of the string s in the array; -1 otherwise.
      */
-    public static int  indexOf(String s, String[] anArray)
-    {
-        //TODO: replace this line with your code.
+    public static int indexOf(String s, String[] anArray) {
+        for (int currentIndex = 0; currentIndex < stringArray.length; currentIndex++) {
+            if (targetString.equals(stringArray[currentIndex])) {
+                return currentIndex;
+            }
+        }
+        return -1;
     }
 
     /**
      * Finds all occurrence of string s in anArray and removes them, returning
      * a new array with all occurrences of s removed.
-     * @param s string to search for in array
+     *
+     * @param s       string to search for in array
      * @param anArray array in which to search and remove s
      * @return An array with all occurrences of s removed.
      */
-    public static String[] remove(String s, String[] anArray)
-    {
-        //TODO: replace this line with your code.
+    public static String[] remove(String s, String[] anArray) {
+        int remainingElementCount = 0;
+
+        for (String currentElement : stringArray) {
+            if (!targetString.equals(currentElement)) {
+                remainingElementCount++;
+            }
+        }
+
+        String[] filteredArray = new String[remainingElementCount];
+        int newArrayIndex = 0;
+
+        for (String currentElement : stringArray) {
+            if (!targetString.equals(currentElement)) {
+                filteredArray[newArrayIndex++] = currentElement;
+            }
+        }
+        return filteredArray;
     }
 
     /**
      * Reverses an array of integers.
+     *
      * @param anArray the array whose contents should be reversed.
      */
-    public static void reverse(int[] anArray)
-    {
-        //TODO: replace this line with your code.
+    public static void reverse(int[] anArray) {
+        int leftIndex = 0;
+        int rightIndex = integerArray.length - 1;
+
+        while (leftIndex < rightIndex) {
+            int temporaryValue = integerArray[leftIndex];
+            integerArray[leftIndex] = integerArray[rightIndex];
+            integerArray[rightIndex] = temporaryValue;
+
+            leftIndex++;
+            rightIndex--;
+        }
     }
 }
